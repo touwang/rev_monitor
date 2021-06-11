e5f7c97929dd53c3441d406904626616dcb12b36
Wright Liu <wright.liu@sap.com>
2021-06-11 17:12:05 +0800

    update the fetch data directly

--- dailyMonitor.py
+++ dailyMonitor.py
@@ -5,52 +5,48 @@ import os
 import mysql.connector
 import shutil
 from datetime import datetime
-import time
+import requests
 
 config = {
-  'user': 'wright',
-  'password': 'P@ssw0rd',
-  'host': '193.123.240.188',
-  'database': 'rchain_monitor',
+  'user': '',
+  'password': '',
+  'host': '',
+  'database': '',
   'raise_on_warnings': True
 }
 
-dataFolder="data/"
 
+today=datetime.today()
 cnx = mysql.connector.connect(**config)
-
 cursor = cnx.cursor()
 
-def moveProcessedFile(filename):
-    shutil.move(dataFolder + filename, dataFolder + "backup/" + filename)
-
-def processAccountInfo(filename):
-    with open(dataFolder + filename, 'r') as f:
-        data = json.load(f)['data']
-        saveData = ("INSERT INTO account_overview "
-               "(total_accounts, 1d_active_account, 1w_active_account, 1m_active_account, top10, top50, top100) "
-               "VALUES (%s, %s, %s, %s, %s, %s, %s)")
-        accountInfo = (data['totalAccount'], data['last24hActiveAccountAmount'], data['last7dActiveAccountAmount'], data['last1mActiveAccountAmount'], data['top10']/100000000, data['top50']/100000000, data['top100']/100000000)
-        cursor.execute(saveData, accountInfo)
-    moveProcessedFile(filename)
+def processAccountInfo():
+    resp = requests.get('https://revdefine.io/define/api/stat/accounts')
+    data = resp.json()['data']
+    saveData = ("INSERT INTO account_overview "
+           "(total_accounts, 1d_active_account, 1w_active_account, 1m_active_account, top10, top50, top100) "
+           "VALUES (%s, %s, %s, %s, %s, %s, %s)")
+    accountInfo = (data['totalAccount'], data['last24hActiveAccountAmount'], data['last7dActiveAccountAmount'], data['last1mActiveAccountAmount'], data['top10']/100000000, data['top50']/100000000, data['top100']/100000000)
+    cursor.execute(saveData, accountInfo)
     return
 
-def processMxcInfo(filename):
-    with open(dataFolder + filename, 'r') as f:
-        data = json.load(f)['account']
-        saveData = ("INSERT INTO daily_account_monitor "
-               "(address, date, balance) "
-               "VALUES (%s, %s, %s)")
-        addressInfo = (data['address'], filename.split(".")[2], data['balance']/100000000)
-        cursor.execute(saveData, addressInfo)
-    moveProcessedFile(filename)
+def processMxcInfo():
+    resp = requests.get('https://revdefine.io/define/api/revaccount/1111fTFCBE727Ex5AHDhAD38HyNca66U5vKVCoQDLwauVCY9DDbBX')
+    data = resp.json()['account']
+    saveData = ("INSERT INTO daily_account_monitor "
+           "(address, date, balance) "
+           "VALUES (%s, %s, %s)")
+    addressInfo = (data['address'], today.strftime('%Y-%m-%d'), data['balance']/100000000)
+    cursor.execute(saveData, addressInfo)
     return
 
-def processMxcTransaction(filename):
-    date = filename.split(".")[2]
-    today = datetime.strptime(date, '%Y-%m-%d')
-    with open(dataFolder + filename, 'r') as f:
-        transactions = json.load(f)['transactions']
+def processMxcTransaction():
+    page=0
+    nextPage=True
+    while nextPage:
+        page += 1
+        resp = requests.get('https://revdefine.io/define/api/transactions/1111fTFCBE727Ex5AHDhAD38HyNca66U5vKVCoQDLwauVCY9DDbBX?rowsPerPage=50&page=' + str(page))
+        transactions = resp.json()['transactions']
         saveData = ("INSERT INTO daily_account_transactions "
                "(blockNumber, fromAddr, toAddr, amount, timestamp) "
                "VALUES (%s, %s, %s, %s, %s)")
@@ -59,32 +55,27 @@ def processMxcTransaction(filename):
             if transaction["transactionType"] == 'transfer' and transactionData > today:
                 transactionInfo = (transaction["blockNumber"], transaction["fromAddr"], transaction["toAddr"], transaction["amount"]/10000000, transaction["timestamp"])
                 cursor.execute(saveData, transactionInfo)
-    moveProcessedFile(filename)
+            elif transactionData < today:
+                nextPage=False
     return
 
-def processTop100(filename):
-    date = filename.split(".")[2]
-    with open(dataFolder + filename, 'r') as f:
-        accounts = json.load(f)['accounts']
-        saveData = ("INSERT INTO daily_top_accounts "
-               "(date, account, balance) "
-               "VALUES (%s, %s, %s)")
-        for account in accounts:
-            accountInfo = (date, account['address'], account['balance']/100000000)
-            cursor.execute(saveData, accountInfo)
-    moveProcessedFile(filename)
+def processTop100():
+    resp = requests.get('https://revdefine.io/define/api/revaccounts?rowsPerPage=100&page=1')
+    accounts = resp.json()['accounts']
+    saveData = ("INSERT INTO daily_top_accounts "
+           "(date, account, balance) "
+           "VALUES (%s, %s, %s)")
+    for account in accounts:
+        accountInfo = (today.strftime('%Y-%m-%d'), account['address'], account['balance']/100000000)
+        cursor.execute(saveData, accountInfo)
     return
 
 
-for filename in os.listdir(dataFolder):
-    if filename.startswith("accountInfo"): 
-        processAccountInfo(filename)
-    elif filename.startswith("mxcInfo"):
-        processMxcInfo(filename)
-    elif filename.startswith("mxcTransaction"):
-        processMxcTransaction(filename)
-    elif filename.startswith("top100"):
-        processTop100(filename)
+
+processAccountInfo()
+processMxcInfo()
+processMxcTransaction()
+processTop100()
 
 cnx.commit()
 cursor.close()
